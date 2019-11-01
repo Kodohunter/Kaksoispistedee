a499ae9816e10c7022891a340bf92ca3af4e8076
Lassi <lassi.hirvonen@pp.inet.fi>
2019-11-01 12:04:06 +0200

    Version 1.0

---
+++ kaksoipistedee/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kaksoipistedee
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            string outputString = ":";
+
+            Random random = new Random();
+            int outputLength = random.Next(10, 30);
+
+            for(int i = 0; i < outputLength; i++)
+            {
+                int nextCharacter = random.Next(1, 3);
+
+                if(nextCharacter == 1)
+                {
+                    outputString += ':';
+                }
+                else
+                {
+                    outputString += 'D';
+                }
+            }
+
+            outputString += 'D';
+
+            Clipboard.SetText(outputString);
+        }
+    }
+}
